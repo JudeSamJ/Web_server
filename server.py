885504e52d0ce8289ca61f34b0b50dfe430837c3
22008539 <pathurideepesh18@gmail.com>
2023-01-16 14:54:39 +0530

    Success

--- server.py
+++ server.py
@@ -1,33 +1,3 @@
-# Developing a Simple Webserver
-
-# AIM:
-Name:Deepesh
-Reference Number:22008539
-
-# DESIGN STEPS:
-
-## Step 1:
-
-HTML content creation is done
-
-## Step 2:
-
-Design of webserver workflow
-
-## Step 3:
-
-Implementation using Python code
-
-## Step 4:
-
-Serving the HTML pages.
-
-## Step 5:
-
-Testing the webserver
-
-# PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -55,11 +25,4 @@ class HelloHandler(BaseHTTPRequestHandler):
 server_address = ('',80)
 httpd = HTTPServer(server_address, HelloHandler)
 httpd.serve_forever()
-```
-# OUTPUT:
-![OUTPUT](./Image/Output.png)
 
-
-# RESULT:
-
-The program is executed succesfully
